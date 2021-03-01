03e9e43d2d98dbf6c5447f17d5575412244bc66e
Hannes Rauhe <hannesrauhe@users.noreply.github.com>
2021-03-01 10:01:02 +0100

    Update button_pressed.sh

--- events/button_pressed.sh
+++ events/button_pressed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 killall mplayer
-blink1-tool --rgd FF9900 -l1
+blink1-tool --rgb FF9900 -l1
 mplayer -playlist http://streaming.radio.co/s774887f7b/listen.m3u -really-quiet > /dev/null 2> /dev/null < /dev/null &
