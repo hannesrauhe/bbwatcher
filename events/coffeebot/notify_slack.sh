e2b5ce215dbd15639d31bc6d84c983e13d3eef75
Hannes Rauhe <hannesrauhe@users.noreply.github.com>
2021-03-19 08:42:33 +0100

    Update notify_slack.sh

--- events/coffeebot/notify_slack.sh
+++ events/coffeebot/notify_slack.sh
@@ -4,4 +4,4 @@ MSG="It's coffee time!"
 if [ -n "$1" ] ; then
   MSG=${1//\"/\\\"}
 fi
-curl -X POST --data-urlencode "payload={\"username\": \"coffeebot\", \"text\": \"$MSG\", \"icon_emoji\": \":coffee:\"}" $SLACK_HOOK_ADDRESS
+curl -X POST --data-urlencode "payload={\"channel\": \"$CHANNEL\", \"username\": \"coffeebot\", \"text\": \"$MSG\", \"icon_emoji\": \":coffee:\"}" $SLACK_HOOK_ADDRESS
