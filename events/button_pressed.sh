bd00559273b4430bc54818f1f9c1712efab7f0af
Hannes <mail+github@rauhe.eu>
2021-06-18 09:35:00 +0200

    script fixes

--- events/button_pressed.sh
+++ events/button_pressed.sh
@@ -11,11 +11,11 @@ blink1-tool --rgb FF9900 -l1
 mplayer -cache-min 40 -playlist http://streaming.radio.co/s774887f7b/listen.m3u -really-quiet > /dev/null 2> /dev/null < /dev/null &
 
 MSG="It's coffee time. $($DIR/consuloftheday/query.sh)"
-echo $MSG
 if [ $(date +%H) -ge "8" ] && [ $(date +%H) -lt "10" ] ; then
+  echo $MSG
   $DIR/coffeebot/notify_slack.sh "$MSG"
 fi
 
 export JUST_NAME=1
 NAME="$($DIR/consuloftheday/query.sh)"
-python3 $DIR/coffeebot/notify_influx.py "$NAME"
+sudo -u pi python3 $DIR/coffeebot/notify_influx.py "$NAME"
